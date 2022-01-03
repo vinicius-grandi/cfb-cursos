14cdc24c645da0a0b3017b2db753a9117687013f
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-01-03 19:57:16 -0300

    manipulando com map

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+<<<<<<< Updated upstream
 
 function App() {
   return (
     <>
     
+=======
+function App() {
+  const carros = ['HB20', 'Palio Focus', 'Lambourguini','Ferrari', 'Lambreta de 4 rodas', 'Caminhão Pequeno']
+  const listaCarros = carros.map((e, i) => (
+    <ul><li id={i}>{e}</li></ul>
+  ))
+
+  return (
+    <>
+      <h1>Manipulando Listas</h1>
+      <h2>Carros Brabos:</h2>
+      {listaCarros}
+>>>>>>> Stashed changes
     </>
   );
 }

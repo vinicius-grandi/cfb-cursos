68aa1e91ac35659db4197fb89c90cdab442318e2
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-01-03 19:38:22 -0300

    mais

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
 
 function App() {
+  const [log, setLog] = useState(false)
   return (
     <>
       <h1>Condicionais</h1>
+      <button onClick={() => setLog(!log)}>{log?'Logar':'Deslogar'}</button>
+      <p>Você está {log?'Logado':'Deslogado'}</p>
     </>
   );
 }

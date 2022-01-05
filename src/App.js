549d677f216108aad430aebea9fc27cf9f103a89
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-01-05 19:18:14 -0300

    Estudando o hook de efeito do react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+<<<<<<< Updated upstream
+=======
+import { useEffect, useState, useRef } from "react";
+>>>>>>> Stashed changes
 
 function App() {
+  const effectTimesCalled = useRef(0)
+
+  useEffect(() => {
+    ++effectTimesCalled.current
+  })
+
+  const [num, setNum] = useState(0) 
+
   return (
     <>
+<<<<<<< Updated upstream
     
+=======
+      <button onClick={() => setNum(num + 2)}>Click-me</button>
+      <p>O valor de clicks é o dobro do hook effect: {num}</p>
+      <p>O hook foi chamado {effectTimesCalled.current}</p>
+>>>>>>> Stashed changes
     </>
   );
 }

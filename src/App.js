d1db5c64c109adac3e05444b2b15d791c4486eff
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-01-05 19:49:37 -0300

    estudando localStorage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+<<<<<<< Updated upstream
 
 function App() {
   return (
     <>
     
+=======
+import { useState } from 'react';
+
+function App() {
+
+  const [nome, setNome] = useState()
+
+  const armazenar = (chave, valor) => {
+    localStorage.setItem(chave, valor)
+  }
+
+  return (
+    <>
+      <p>Insira seu nome: <input type="text" name="nome" id="nome" onChange={e => {
+        setNome(e.target.value)
+        armazenar("nome", nome)
+      }}/></p>
+      <p>O nome é: {localStorage.getItem("nome")}</p>
+>>>>>>> Stashed changes
     </>
   );
 }

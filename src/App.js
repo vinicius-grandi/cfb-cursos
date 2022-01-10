ea965994df76a80a796e41f5d3f8142a721421a9
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-01-10 19:09:23 -0300

    Aprendendo sobre métodos herdados da classe React.Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import Carro from './componentes/Carro.jsx'
+import { useState } from 'react'
 
 function App() {
+  const [fator, setFator] = useState(0)
+  const [carro, setCarro] = useState(true)
+
   return (
-    <>
-    
+    <>    
+      <h1>Componentes de Classe</h1>
+      
+      {carro ?
+        <>
+          <Carro fator={Number(fator)}/>
+          <p>Fator de velocidade<input type="number" onChange={(e) => setFator(e.target.value)}/></p>
+        </>
+       : <p>F para o carrinho</p>}
+
+      <p>Existência do Carro: <button onClick={() => setCarro(!carro)}>
+        Sumir/Aparecer</button></p>
     </>
   );
 }
---
+++ src/componentes/Carro.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+class Carro extends React.Component {
+    constructor() {
+        super()
+        this.modelo='Golf'
+        this.state = {
+            ligado: false,
+            velAtual: 0,
+        }
+        
+    }
+
+    ligar() {
+        this.setState(state => ({
+            ligado:!state.ligado,
+            velAtual: 0
+        }))
+    }
+
+    acelerar() {
+        if (!this.state.ligado) return alert("O carro está desligado!")
+        this.setState((state, props) => ({
+            velAtual: state.velAtual + props.fator
+        }))
+    }
+
+    render() {
+        return (
+            <>
+                <h1>Meu carro</h1>
+                <p>{this.modelo}</p>
+                <p>O carro está {(this.state.ligado?"Ligado":"Desligado")} e sua velocidade é {this.state.velAtual}</p>
+                <button onClick={() => this.ligar()}>Ligar/Desligar</button>
+                <button onClick={() => this.acelerar()}>Acelerar</button>
+            </>
+        )
+    }
+
+    componentDidMount() {
+        console.log('Carro montado vrum vrum')
+    }
+
+    componentDidUpdate() {
+        console.log('O carro está sendo alterado OMG!')
+    }
+
+    componentWillUnmount() {
+        console.log('O carro foi esmigalhado')
+    }
+}
+
+export default Carro

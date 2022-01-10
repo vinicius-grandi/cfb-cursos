3c6367398880060e722af85fd7b52771589ead62
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-01-10 18:09:59 -0300

    Outra forma de usar state em classes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { useState } from "react";
+import Carro from "./componentes/Carro";
 
 function App() {
+  const [fator, setFator] = useState(0)
   return (
-    <>
-    
+    <>    
+      <h1>Componentes de Classe</h1>
+      <Carro fator={Number(fator)}/>
+      <p>Fator de velocidade<input type="number" onChange={(e) => setFator(e.target.value)}/></p>
     </>
   );
 }

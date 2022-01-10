8d1fd148a8e52318204857cb63993cb1e367e131
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-01-10 16:35:34 -0300

    resolvendo problemas no arquivo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-<<<<<<< Updated upstream
-=======
 import Classe from "./componentes/Classe";
->>>>>>> Stashed changes
 
 function App() {
   return (
-    <>
-    
-=======
+    <>    
       <h1>Componentes de Classe</h1>
       <Classe nome="Valdir"/>
->>>>>>> Stashed changes
+
     </>
   );
 }
 
-export default App;
 export default App;

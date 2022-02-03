68e196853127bf6ad1ec7bdf874cebc3e104cd5d
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-02-03 09:50:50 -0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import { useState } from 'react'
 
 function App() {
+
+  const [info, setInfo] = useState({peso:0, altura:0, resultado:0})
+
   return (
     <>
-    
+      <p><label htmlFor="peso">Informe seu peso: <input 
+        type="number" 
+        id="peso" 
+        onChange={e => setInfo({...info, peso:e})}/></label></p>
+
+      <p><label htmlFor="altura">Informe sua altura: <input 
+        type="number" 
+        id="altura" 
+        onChange={e => setInfo({...info, altura:e})} /></label></p>
+      <button>Calcular</button>
+
+      <p>Resultado: {info.altura / info.peso}</p>
+
+      <table>
+        <thead>
+          <th>Classificação</th>
+          <th>IMC</th>
+        </thead>
+        <tbody>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+        </tbody>
+      </table>
     </>
   );
 }
